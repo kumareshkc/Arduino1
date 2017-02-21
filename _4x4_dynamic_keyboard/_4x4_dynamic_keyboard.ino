63bc1c8b0a7218d9bf08a991ca06eec6ad229fe3
kumareshkc <kumareshkc1994@gmail.com>
2017-02-21 22:22:19 +0530

    Add files via upload

---
+++ _4x4_dynamic_keyboard/_4x4_dynamic_keyboard.ino
@@ -0,0 +1,131 @@
+#include <Keypad.h>
+#include <ctype.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+// Define the keymaps.  The blank spot (lower left) is the space character.
+char alphaKeys[ROWS][COLS] = {
+    { 'a','d','g',',' },
+    { 'j','m','p','{' },
+    { 's','v','y','}'},
+    { ' ','.','#','!'}
+};
+
+char numberKeys[ROWS][COLS] = {
+    {'1','2','3',','},
+    {'4','5','6','{'},
+    {'7','8','9','}'},
+    {'*','0','#','!'},
+};
+
+boolean alpha = false;   // Start with the numeric keypad.
+
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+
+// Create two new keypads, one is a number pad and the other is a letter pad.
+Keypad numpad( makeKeymap(numberKeys), rowPins, colPins, sizeof(rowPins), sizeof(colPins) );
+Keypad ltrpad( makeKeymap(alphaKeys), rowPins, colPins, sizeof(rowPins), sizeof(colPins) );
+
+
+unsigned long startTime;
+const byte ledPin = 13;                            // Use the LED on pin 13.
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);                 // Turns the LED on.
+    ltrpad.begin( makeKeymap(alphaKeys) );
+    numpad.begin( makeKeymap(numberKeys) );
+    ltrpad.addEventListener(keypadEvent_ltr);  // Add an event listener.
+    ltrpad.setHoldTime(500);                   // Default is 1000mS
+    numpad.addEventListener(keypadEvent_num);  // Add an event listener.
+    numpad.setHoldTime(500);                   // Default is 1000mS
+}
+
+char key;
+
+void loop() {
+
+    if( alpha )
+        key = ltrpad.getKey( );
+    else
+        key = numpad.getKey( );
+
+    if (alpha && millis()-startTime>100) {           // Flash the LED if we are using the letter keymap.
+        digitalWrite(ledPin,!digitalRead(ledPin));
+        startTime = millis();
+    }
+}
+
+static char virtKey = NO_KEY;      // Stores the last virtual key press. (Alpha keys only)
+static char physKey = NO_KEY;      // Stores the last physical key press. (Alpha keys only)
+static char buildStr[12];
+static byte buildCount;
+static byte pressCount;
+
+static byte kpadState;
+
+// Take care of some special events.
+
+void keypadEvent_ltr(KeypadEvent key) {
+    // in here when in alpha mode.
+    kpadState = ltrpad.getState( );
+    swOnState( key );
+} // end ltrs keypad events
+
+void keypadEvent_num( KeypadEvent key ) {
+    // in here when using number keypad
+    kpadState = numpad.getState( );
+    swOnState( key );
+} // end numbers keypad events
+
+void swOnState( char key ) {
+    switch( kpadState ) {
+        case PRESSED:
+            if (isalpha(key)) {              // This is a letter key so we're using the letter keymap.
+                if (physKey != key) {        // New key so start with the first of 3 characters.
+                    pressCount = 0;
+                    virtKey = key;
+                    physKey = key;
+                }
+                else {                       // Pressed the same key again...
+                    virtKey++;                   // so select the next character on that key.
+                    pressCount++;                // Tracks how many times we press the same key.
+                }
+                    if (pressCount > 2) {    // Last character reached so cycle back to start.
+                        pressCount = 0;
+                        virtKey = key;
+                    }
+                    Serial.print(virtKey);   // Used for testing.
+                }
+                if (isdigit(key) || key == ' ' || key == '.')
+                    Serial.print(key);
+                if (key == '#')
+                    Serial.println();
+                break;
+
+        case HOLD:
+            if (key == '#')  {               // Toggle between keymaps.
+                if (alpha == true)  {        // We are currently using a keymap with letters
+                    alpha = false;           // Now we want a keymap with numbers.
+                    digitalWrite(ledPin, LOW);
+                }
+                else  {                      // We are currently using a keymap with numbers
+                    alpha = true;            // Now we want a keymap with letters.
+                }
+            }
+            else  {                          // Some key other than '#' was pressed.
+                buildStr[buildCount++] = (isalpha(key)) ? virtKey : key;
+                buildStr[buildCount] = '\0';
+                Serial.println();
+                Serial.println(buildStr);
+            }
+            break;
+
+        case RELEASED:
+            if (buildCount >= sizeof(buildStr))  buildCount = 0;  // Our string is full. Start fresh.
+            break;
+    }  // end switch-case
+}// end switch on state function
+
---
+++ _7_SEGMENT_COUNT_DOWN/_7_SEGMENT_COUNT_DOWN.ino
@@ -0,0 +1,110 @@
+int pinA = 2;
+int pinB = 3;
+int pinC = 4;
+int pinD = 5;
+int pinE = 6;
+int pinF = 7;
+int pinG = 8;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(pinA,OUTPUT);
+pinMode(pinB,OUTPUT);
+pinMode(pinC,OUTPUT);
+pinMode(pinD,OUTPUT);
+pinMode(pinE,OUTPUT);
+pinMode(pinF,OUTPUT);
+pinMode(pinG,OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  // 9
+digitalWrite(pinA,LOW);
+digitalWrite(pinB,LOW);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,HIGH);
+digitalWrite(pinE,HIGH);
+digitalWrite(pinF,LOW);
+digitalWrite(pinG,LOW);
+delay(1000);
+  // 8
+digitalWrite(pinA,LOW);
+digitalWrite(pinB,LOW);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,LOW);
+digitalWrite(pinE,LOW);
+digitalWrite(pinF,LOW);
+digitalWrite(pinG,LOW);
+delay(1000);
+  // 7
+digitalWrite(pinA,LOW);
+digitalWrite(pinB,LOW);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,HIGH);
+digitalWrite(pinE,HIGH);
+digitalWrite(pinF,HIGH);
+digitalWrite(pinG,HIGH);
+delay(1000);
+  // 6
+digitalWrite(pinA,LOW);
+digitalWrite(pinB,HIGH);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,LOW);
+digitalWrite(pinE,LOW);
+digitalWrite(pinF,LOW);
+digitalWrite(pinG,LOW);
+delay(1000);
+ // 5
+digitalWrite(pinA,LOW);
+digitalWrite(pinB,HIGH);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,LOW);
+digitalWrite(pinE,HIGH);
+digitalWrite(pinF,LOW);
+digitalWrite(pinG,LOW);
+delay(1000);
+  // 4
+digitalWrite(pinA,HIGH);
+digitalWrite(pinB,LOW);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,HIGH);
+digitalWrite(pinE,HIGH);
+digitalWrite(pinF,LOW);
+digitalWrite(pinG,LOW);
+delay(1000);
+  // 3
+digitalWrite(pinA,LOW);
+digitalWrite(pinB,LOW);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,LOW);
+digitalWrite(pinE,HIGH);
+digitalWrite(pinF,HIGH);
+digitalWrite(pinG,LOW);
+delay(1000);
+  // 2
+digitalWrite(pinA,LOW);
+digitalWrite(pinB,LOW);
+digitalWrite(pinC,HIGH);
+digitalWrite(pinD,LOW);
+digitalWrite(pinE,LOW);
+digitalWrite(pinF,HIGH);
+digitalWrite(pinG,LOW);
+delay(1000);
+  // 1
+digitalWrite(pinA,HIGH);
+digitalWrite(pinB,LOW);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,HIGH);
+digitalWrite(pinE,HIGH);
+digitalWrite(pinF,HIGH);
+digitalWrite(pinG,HIGH);
+delay(1000);
+  //0
+digitalWrite(pinA,LOW);
+digitalWrite(pinB,LOW);
+digitalWrite(pinC,LOW);
+digitalWrite(pinD,LOW);
+digitalWrite(pinE,LOW);
+digitalWrite(pinF,LOW);
+digitalWrite(pinG,HIGH);
+delay(1000);
+}
